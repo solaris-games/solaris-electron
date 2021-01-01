34cc63b0857d6b6ef99522622383d811ea86edf1
Mike Eason <mike.eason@mail.com>
2021-01-01 21:00:29 +0000

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Solaris Desktop Client
+
+This is an Electron wrapper around the [Solaris repository](https://github.com/mike-eason/solaris). This repository is used to build the desktop client for the game.
+
+## Contributing
+See [here](CONTRIBUTING.md).
+
+## License
+See [here](LICENSE).
---
+++ index.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  let url = process.env.NODE_ENV === 'DEV' ? 'http://localhost:8080' : 'https://solaris.games'
+  
+  win.loadURL(url);
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
